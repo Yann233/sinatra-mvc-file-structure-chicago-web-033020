f387cefcaf5111443ed35b4b6314bf132afb5b04
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-20 16:13:27 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,12 +2,12 @@ class Dog
     @@all = []
     attr_accessor :name, :breed, :age
     def initialize(name, breed, age)
-        @name = name 
-        @breed = breed 
-        @age = age 
-        @@all << self 
-    end 
-    def self.all 
-        @@all 
-    end 
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 end
